0ffcf35b08c7592da7c5551f36996ddae4b22916
Jedro1270 <jedro1270@gmail.com>
2021-03-07 09:51:45 +0800

    feat: replace palceholders and add header

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Toolbar, styled, Typography, Box, IconButton } from '@material-ui/core';
+import { Menu } from '@material-ui/icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, this is JMPlus
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderBody position="static">
+        <HeaderContents> 
+
+          <IconButton>
+            <MenuIcon/>
+          </IconButton>
+
+          <Logo>
+            <HeaderTitle>
+              Popeye Resto
+            </HeaderTitle>
+            <img src="popeye-logo.png" alt="Popeye Logo" height="100%"/>
+          </Logo>
+
+          <Box/>
+
+        </HeaderContents>
+
+      </HeaderBody>
     </div>
   );
 }
+
+const HeaderBody = styled(AppBar)({
+  backgroundColor: 'red'
+});
+
+const HeaderContents = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between'
+});
+
+const HeaderTitle = styled(Typography)({
+  fontSize: '40px',
+  fontWeight: 'bold',
+  textAlign: 'center'
+});
+
+const Logo = styled(Box)({
+  height: '100px',
+  margin: '10px',
+  display: 'flex',
+  flexDirection: 'row'
+});
+
+const MenuIcon = styled(Menu)({
+  fontSize: '50px',
+  color: 'white'
+});
 
 export default App;
