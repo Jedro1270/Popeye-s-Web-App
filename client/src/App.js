b5a3e8b2ed31c66750dec3fbff86450f33487564
Jedro1270 <jedro1270@gmail.com>
2021-03-16 10:44:59 +0800

    fix: fix layout for mobile

--- client/src/App.js
+++ client/src/App.js
@@ -8,52 +8,52 @@ function App() {
   const [meals, setMeals] = useState([
     {
       imagePath: '/testing/sample-food-1.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       price: 100
     },
     {
       imagePath: '/testing/sample-food-2.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. ',
       price: 150
     },
     {
       imagePath: '/testing/sample-food-3.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. ',
       price: 200
     },
     {
       imagePath: '/testing/sample-food-1.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, ',
       price: 150
     },
     {
       imagePath: '/testing/sample-food-2.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
       price: 180
     },
     {
       imagePath: '/testing/sample-food-3.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. ',
       price: 118
     },
     {
       imagePath: '/testing/sample-food-1.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, ',
       price: 199
     },
     {
       imagePath: '/testing/sample-food-2.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. ',
       price: 299
     },
     {
       imagePath: '/testing/sample-food-3.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. ',
       price: 215
     },
     {
       imagePath: '/testing/sample-food-1.jpg',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, eros nunc tempor eros, vitae dignissim ligula elit id lectus. Cras luctus id nulla tempus efficitur. In bibendum diam quis dolor aliquam euismod. Donec ac commodo nisl. Donec convallis tellus vitae erat consequat, id posuere ante pellentesque. Aliquam fermentum, neque at auctor molestie, elit lorem posuere tellus, non euismod leo dolor vel felis. Aenean venenatis pharetra ante, a efficitur est mattis eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla condimentum id diam vel lacinia. Mauris a leo tincidunt, placerat quam sit amet, commodo lectus. Fusce consequat dui mauris, eu ultricies felis ultrices ornare. Phasellus posuere justo eget enim aliquam viverra. Curabitur vehicula facilisis vulputate.',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem dui. Ut bibendum, eros sit amet consequat tincidunt, ',
       price: 100
     },
   ]); // For testing, set to [] for production
@@ -109,14 +109,20 @@ const HeaderContents = styled(Toolbar)({
 const HeaderTitle = styled(Typography)({
   fontSize: '40px',
   fontWeight: 'bold',
-  textAlign: 'center'
+  textAlign: 'center',
+  '@media (max-width: 1024px)': {
+    fontSize: '30px'
+  }
 });
 
 const Logo = styled(Box)({
   height: '100px',
   margin: '10px',
   display: 'flex',
-  flexDirection: 'row'
+  flexDirection: 'row',
+  '@media (max-width: 1024px)': {
+    height: '75px'
+  },
 });
 
 const MenuIcon = styled(Menu)({
